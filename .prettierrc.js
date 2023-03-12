39787456a27b70e1afe4405d06a0e86f14937fba
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-03-12 11:21:22 +0800

    feat: add prettier and extend eslint

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports= {
+    pluginSearchDirs: ['.'],
+    trailingComma: 'none',
+    semi: true,
+    singleQuote: true,
+    printWidth: 100,
+    useTabs: false,
+    tabWidth: 4,
+    quoteProps: 'consistent',
+    plugins: ['@trivago/prettier-plugin-sort-imports'],
+    importOrder: ['<THIRD_PARTY_MODULES>', '^@/', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import Image from 'next/image';
+
 // import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <>
-      <main>
-    hah
-      </main>
-    </>
-  )
+    return (
+        <>
+            <main>Hi, welcome home.</main>
+        </>
+    );
 }
