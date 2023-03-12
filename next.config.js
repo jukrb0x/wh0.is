a00feab641aec903ba923772f021b8f086ef9d9b
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-03-12 11:31:43 +0800

    style: reformat configs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true
+};
 
 const withNextra = require('nextra')({
-  theme: 'nextra-theme-blog',
-  themeConfig: './theme.config.jsx',
-})
+    theme: 'nextra-theme-blog',
+    themeConfig: './theme.config.jsx'
+});
 
-module.exports = /* nextConfig */withNextra(nextConfig)
+module.exports = /* nextConfig */ withNextra(nextConfig);
