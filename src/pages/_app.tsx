a3522a5a1c1e731b98361f835200a7cfe1bac016
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-03-13 13:49:18 +0800

    feat: add UnoCSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 // import '@/styles/globals.css'
 import type { AppProps } from 'next/app';
+import 'uno.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+        </>
+    );
 }
