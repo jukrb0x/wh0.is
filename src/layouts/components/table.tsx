ef70e0158d9f68467a8987f327bbea97a3872058
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-03-16 18:37:04 +0800

    wip: decouple default components

---
+++ src/layouts/components/table.tsx
@@ -0,0 +1,5 @@
+import type { ComponentProps } from 'react';
+
+export const Table = ({ className = '', ...props }: ComponentProps<'table'>) => (
+    <table className={'nx-block nx-overflow-x-scroll ' + className} {...props} />
+);
