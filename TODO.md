7e9f434c409cf7e9d5b1e65d0914006f39c61232
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-03-16 18:13:05 +0800

    fix: tailwind css reset

--- TODO.md
+++ TODO.md
@@ -2,3 +2,8 @@ Problems I met during the development:
 
 1. neovim tresitter support for mdx is not ok
 2. prettier will consider mdx as markdown file so that the comment block won't work after formatting
+
+Roadmap:
+
+-   [ ] code block highlighting
+-   [ ] mdx styles are broken
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-// import '@/styles/globals.css'
+import '@unocss/reset/tailwind.css';
 import type { AppProps } from 'next/app';
 import 'uno.css';
 
 import '@/styles/main.css';
 import '@/styles/markdown.css';
-import '@/styles/nextra-defaults.css';
+import '@/styles/nextra-defaults.scss';
 import '@/styles/prose.css';
 
 export default function App({ Component, pageProps }: AppProps) {
