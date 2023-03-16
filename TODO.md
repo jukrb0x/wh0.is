b22729e3a5258000c1300246b1da9a779413728b
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-03-17 02:26:38 +0800

    wip: transformative `@apply` works now but
    unocs is broken due to nextra (guess)

--- TODO.md
+++ TODO.md
@@ -2,6 +2,7 @@ Problems I met during the development:
 
 1. neovim tresitter support for mdx is not ok
 2. prettier will consider mdx as markdown file so that the comment block won't work after formatting
+3. the latest `@unocss/postcss` plugin can cause stucking at dev compiling
 
 Roadmap:
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,12 @@ import '@unocss/reset/tailwind.css';
 import type { AppProps } from 'next/app';
 import 'uno.css';
 
-import '@/styles/global.css';
-import '@/styles/main.css';
-import '@/styles/markdown.css';
-import '@/styles/nextra-defaults.scss';
-import '@/styles/prose.css';
-import '@/styles/shiki.css';
+import '@/styles/code-block.scss';
+import '@/styles/subheading-anchor.scss';
+
+import '../styles/global.css';
+import '../styles/shiki.css';
+import '../styles/style.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
