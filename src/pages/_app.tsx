d955f0169ae5cb8b3038e92996bc7a9d7ccdd8db
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-03-16 18:49:35 +0800

    feat: decouple nextra theme and mdx components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import '@/styles/main.css';
 import '@/styles/markdown.css';
 import '@/styles/nextra-defaults.scss';
 import '@/styles/prose.css';
+import '@/styles/shiki.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
