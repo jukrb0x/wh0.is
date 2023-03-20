2ec9556849f420477336658a246d76fb87640c86
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-03-20 12:35:06 +0800

    fix: code block css must go in app entry

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import '@unocss/reset/tailwind.css';
 import type { AppProps } from 'next/app';
 import 'uno.css';
 
+import '@/styles/code-block.scss';
 import '@/styles/global.scss';
-import '@/styles/shiki.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
