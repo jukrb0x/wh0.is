ca52b415450f9d664bde9ee96fd096ca60fc92bb
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-03-29 01:56:43 +0800

    Revert "fix code block highlighting in dark mode 🌔 "

--- src/layouts/components/code.tsx
+++ src/layouts/components/code.tsx
@@ -6,15 +6,12 @@ export const Code = ({
     ...props
 }: ComponentProps<'code'>): ReactElement => {
     const hasLineNumbers = 'data-line-numbers' in props;
-    console.log(props);
-    const isCodeBlock = 'data-language' in props || 'data-theme' in props;
     return (
         <code
             className={[
                 'border-black border-opacity-[0.04] bg-opacity-[0.03] bg-black break-words rounded-md border py-0.5 px-[.25em] text-[.9em]',
-                'dark:border-white/10',
+                'dark:border-white/10 dark:bg-white/10',
                 hasLineNumbers ? '[counter-reset:line]' : '',
-                isCodeBlock ? '' : 'dark:bg-white/10',
                 className
             ].join(' ')}
             // always show code blocks in ltr
