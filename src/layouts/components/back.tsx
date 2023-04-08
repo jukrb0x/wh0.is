0f5e0c4b2145f1a4bef8d5f5f153f2e756be3ca7
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-04-08 21:44:52 +0800

    fix: accessibility text for `cd`

--- src/layouts/components/back.tsx
+++ src/layouts/components/back.tsx
@@ -4,7 +4,9 @@ export const Back = ({ href }: { href: string }) => (
     <div className={'prose dark:prose-invert m-auto mt-8 mb-8'}>
         <span className={'font-mono op50'}>{'> '}</span>
         <Link href={href} passHref legacyBehavior>
-            <a className={'font-mono op50 hover:op75'}>cd ..</a>
+            <a className={'font-mono op50 hover:op75'} title={'Go Back'}>
+                cd ..
+            </a>
         </Link>
     </div>
 );
