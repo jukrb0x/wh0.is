7000a4563c8fc89e195271f8c83bd8f70e9ac99a
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-05-07 16:13:17 +0800

    feat: add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 import 'uno.css';
 import 'yet-another-react-lightbox/styles.css';
 
+import { NProgress } from '@/layouts/components/progress-indicator';
 import '@/styles/code-block.scss';
 import '@/styles/global.scss';
 import '@/styles/main.scss';
@@ -11,6 +12,7 @@ import '@/styles/main.scss';
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <NProgress />
             <Component {...pageProps} />
             <Analytics />
         </>
