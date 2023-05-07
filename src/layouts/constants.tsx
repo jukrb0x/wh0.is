37f5780f03d2e455892c538a1d78135196deec28
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-05-07 13:11:12 +0800

    fix: cusdis styles

--- src/layouts/constants.tsx
+++ src/layouts/constants.tsx
@@ -1,8 +1,10 @@
 import path from 'path';
 
+import Comments from './cusdis';
 import type { NextraBlogThemeConfig } from './types';
 
 export const DEFAULT_THEME_CONFIG: NextraBlogThemeConfig = {
+    comments: <Comments />,
     footer: (
         <small className="mt-32 block">CC BY-NC 4.0 {new Date().getFullYear()} © Jabriel</small>
     ),
