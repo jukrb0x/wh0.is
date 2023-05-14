ef8c3e17b67be2540519c7715f7a709a7fbe45c2
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-05-15 00:15:13 +0800

    fix: make table display looks good

--- src/layouts/components/table.tsx
+++ src/layouts/components/table.tsx
@@ -1,5 +1,6 @@
+import clsx from 'clsx';
 import type { ComponentProps } from 'react';
 
 export const Table = ({ className = '', ...props }: ComponentProps<'table'>) => (
-    <table className={'block overflow-x-scroll ' + className} {...props} />
+    <table className={'overflow-x-scroll ' + className} {...props} />
 );
