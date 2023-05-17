86b4473bd9d5bdf345d38b364ae0bf3053280e39
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-05-17 19:43:53 +0800

    fix: move table wrapper style to jsx

--- src/layouts/components/table.tsx
+++ src/layouts/components/table.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import type { ComponentProps } from 'react';
 
 export const Table = ({ className = '', ...props }: ComponentProps<'table'>) => (
-    <div className={'table-wrapper block w-full overflow-x-scroll'}>
+    <div className={'table-wrapper block w-full my-[2rem] overflow-x-scroll'}>
         <table className={className} {...props} />
     </div>
 );
